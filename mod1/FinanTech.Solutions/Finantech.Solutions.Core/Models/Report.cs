d06129e7b0d71d218519fe7711463cb2e1016401
JesusFerru <a2020115239@estudiantes.upsa.edu.bo>
2026-05-29 23:13:33 -0400

    feat: add users and channels. Add show report message

--- mod1/FinanTech.Solutions/Finantech.Solutions.Core/Models/Report.cs
+++ mod1/FinanTech.Solutions/Finantech.Solutions.Core/Models/Report.cs
@@ -20,6 +20,22 @@ public class Report : IReportComponent
 
     public string Export()
     {
-        throw new NotImplementedException();
+        var showReport = new StringBuilder();
+        showReport.AppendLine($"=== REPORT FORMAT: {Format} ===\n");
+        showReport.AppendLine($"Reporte: {Title}");
+        if (!string.IsNullOrEmpty(EncryptedCode)) showReport.AppendLine($"Codigo: {EncryptedCode}");
+        showReport.AppendLine($"Fecha: {GenerationDate:yyyy-MM-dd}");
+        showReport.AppendLine($"Contenido: {Content}");
+
+        if (!string.IsNullOrEmpty(DetailedInformation)) showReport.AppendLine($"Info detallada: {DetailedInformation}");
+        if (ChartsList.Count > 0) showReport.AppendLine($"Listado de gráficos: \n - {string.Join("\n - ", ChartsList)}");
+
+        showReport.AppendLine($"Conclusiones: {Conclusions}");
+        showReport.AppendLine($"Firma Responsable: {ResponsibleSignature}");
+
+        if (Annexes.Count > 0) showReport.AppendLine($"Anexos: \n - {string.Join("\n - ", Annexes)}");
+        showReport.AppendLine("=================================");
+
+        return showReport.ToString();
     }
 }
