52674ea57f82fa3b46731a8e4c8cb4ab7ae60d77
Luis Jesus Ferrufino <a2020115239@estudiantes.upsa.edu.bo>
2026-09-26 07:15:24 -0400

    feat: complete testing to microservices

--- Nurtricenter/Nurtricenter.MS3.Application/DependencyInjection.cs
+++ Nurtricenter/Nurtricenter.MS3.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nurtricenter.MS3.Application.Simulations;
 
@@ -8,14 +9,26 @@ namespace Nurtricenter.MS3.Application;
 /// </summary>
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration? configuration = null)
     {
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
         });
 
-        services.AddSingleton<ISimulationService, SimulationDataService>();
+        var mockBaseUrl = configuration?["ExternalServices:SimulationMockBaseUrl"];
+
+        if (!string.IsNullOrWhiteSpace(mockBaseUrl))
+        {
+            services.AddHttpClient<ISimulationService, HttpSimulationService>(client =>
+            {
+                client.BaseAddress = new Uri(mockBaseUrl);
+            });
+        }
+        else
+        {
+            services.AddSingleton<ISimulationService, SimulationDataService>();
+        }
 
         return services;
     }
